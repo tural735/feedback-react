cbf880424d675513496047423e80313d3848e30c
Tural Memmedzade <49388337+tural735@users.noreply.github.com>
2022-02-28 02:12:14 +0400

    add json-server and concurrently package and add fetch with useEffect and Loading component

--- src/components/FeedBackList.js
+++ src/components/FeedBackList.js
@@ -3,13 +3,14 @@ import { useContext } from 'react';
 import {motion,AnimatePresence} from 'framer-motion'
 import FeedBackItem from "./FeedBackItem";
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 function FeedBackList() {
-  const {feedback}= useContext(FeedbackContext)
-  if (!feedback || feedback.length === 0) {
+  const {feedback,isLoading}= useContext(FeedbackContext)
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>Not feedback yet</p>;
   }
-  return (
+  return isLoading ? (<Spinner/>) :(
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) =>(
@@ -20,7 +21,7 @@ function FeedBackList() {
         )}
       </AnimatePresence>
     </div>
-  );
+  )
   // return (
   //   <div className="feedback-list">
   //     {feedback.map((item) => {
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,15 +1,27 @@
-import { createContext,useState } from "react";
+import { createContext,useState,useEffect } from "react";
 import {v4 as uuidv4} from 'uuid'
-import data from '../data/feedback';
+// import data from '../data/feedback';
 const FeedbackContext=createContext();
 
 
 export const FeedbackProvider=({children})=>{
-    const [feedback,setFeedback]=useState(data);
+    const [isLoading,setIsLoading]=useState(true);
+    const [feedback,setFeedback]=useState([]);
     const [feedbackEdit,setFeedbackEdit]=useState({
         item:{},
         edit:false
     })
+
+    useEffect(()=>{
+        fetchFeedback();
+    },[])
+
+    const fetchFeedback= async()=>{
+        const response= await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+        const data= await response.json();
+        setFeedback(data);
+        setIsLoading(false);
+    }
     // edit Feedback
     const editFeedback=(item)=>{
         setFeedbackEdit({
@@ -44,7 +56,8 @@ export const FeedbackProvider=({children})=>{
             addFeedBack,
             editFeedback,
             feedbackEdit,
-            updateFeedback
+            updateFeedback,
+            isLoading
         }}>
             {children}
         </FeedbackContext.Provider>
